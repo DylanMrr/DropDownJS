f9a8eed6687218e5baac06bf750d57121c32022d
OSINIT\dmitriy.eginov <dmitriy.eginov@osinit.com>
2023-09-21 10:58:11 +0300

    js

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-(function(){
-    const data = ['first', 'second', 'third', ['fourth-one', 'fourth-two']];
-
+function init(){
+    const data = ['first', 'second', 'third', ['fourth-one', 'fourth-two'], ['fifth-one', 'fifth-two']];
     const container = document.getElementById('container');
+    const select  = document.createElement('select');
+            
+    const selectName = document.createElement('option')
+    selectName.setAttribute('value', '');
+    selectName.textContent = 'Choose';
+    select.appendChild(selectName);
 
+    container.appendChild(select);
 
-    function init(container){
-        const select  = document.createElement('select');
-        container.appendChild(select);
+    for (const element of data){
+        if (Array.isArray(element)){
+            appendArrayOptions(select, element);
+            continue;
+        }
+                
+        appendOption(select, element);
     }
-    console.log(container)
-    init(container);
-})();
+}
+
+function appendArrayOptions(parent, data){
+    const select  = document.createElement('optgroup');
+    select.setAttribute('label', 'Choose');
+
+    for (const element of data){
+        if (Array.isArray(element)){
+            appendArrayOptions(select, element);
+            continue;
+        }
+
+        appendOption(select, element);
+    }
+    parent.appendChild(select);
+}
+
+function appendOption(parent, data){
+    const option = document.createElement('option');
+    option.setAttribute('value', data);
+    option.textContent = `${data}`;
+    parent.appendChild(option);
+}
+
